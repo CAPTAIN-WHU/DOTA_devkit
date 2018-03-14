2b1be75957cd04376aa13118b5cc9c0f58d4ed44
dingjian <574258651@qq.com>
2018-03-14 11:39:42 +0800

    add SplitOnlyImage

--- SplitOnlyImage.py
+++ SplitOnlyImage.py
@@ -19,7 +19,7 @@ class splitbase():
         self.srcpath = srcpath
         self.dstpath = dstpath
         self.ext = ext
-    def saveimagepatches(self, img, subimgname, left, up, ext='.jpg'):
+    def saveimagepatches(self, img, subimgname, left, up, ext='.png'):
         subimg = copy.deepcopy(img[up: (up + self.subsize), left: (left + self.subsize)])
         outdir = os.path.join(self.dstpath, subimgname + ext)
         cv2.imwrite(outdir, subimg)
@@ -46,7 +46,7 @@ class splitbase():
                 if (up + self.subsize >= height):
                     up = max(height - self.subsize, 0)
                 subimgname = outbasename + str(left) + '___' + str(up)
-                self.saveimagepatches(resizeimg, subimgname, left, up, '.jpg')
+                self.saveimagepatches(resizeimg, subimgname, left, up)
                 if (up + self.subsize >= height):
                     break
                 else:
