0e65afa45519bde72fb9b6d9996209e0353f9c46
dingjian <574258651@qq.com>
2019-05-18 22:54:38 +0800

    tmp save

--- DOTA2COCO.py
+++ DOTA2COCO.py
@@ -26,11 +26,12 @@ def DOTA2COCO(srcpath, destfile):
         data_dict['categories'].append(single_cat)
 
     inst_count = 1
+    image_id = 1
     with open(destfile, 'w') as f_out:
         filenames = util.GetFileFromThisRootDir(labelparent)
         for file in filenames:
             basename = util.custombasename(file)
-            image_id = int(basename[1:])
+            # image_id = int(basename[1:])
 
             imagepath = os.path.join(imageparent, basename + '.png')
             img = cv2.imread(imagepath)
@@ -49,16 +50,19 @@ def DOTA2COCO(srcpath, destfile):
                 single_obj = {}
                 single_obj['area'] = obj['area']
                 single_obj['category_id'] = wordname_15.index(obj['name']) + 1
-                single_obj['segmentation'] = obj['poly']
+                single_obj['segmentation'] = []
+                single_obj['segmentation'].append(obj['poly'])
                 single_obj['iscrowd'] = 0
                 xmin, ymin, xmax, ymax = min(obj['poly'][0::2]), min(obj['poly'][1::2]), \
                                          max(obj['poly'][0::2]), max(obj['poly'][1::2])
 
-                single_obj['bbox'] = xmin, ymin, xmax, ymax
+                width, height = xmax - xmin, ymax - ymin
+                single_obj['bbox'] = xmin, ymin, width, height
                 single_obj['image_id'] = image_id
                 data_dict['annotations'].append(single_obj)
                 single_obj['id'] = inst_count
                 inst_count = inst_count + 1
+            image_id = image_id + 1
         json.dump(data_dict, f_out)
 if __name__ == '__main__':
-    DOTA2COCO(r'/home/dingjian/data/dota/val', r'/home/dingjian/data/dota/val/DOTA_val.json')
+    DOTA2COCO(r'/data0/data_dj/1024_new', r'/data0/data_dj/1024_new/DOTA_trainval1024.json')
